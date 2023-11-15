dc70b9e412c3e6ea35a5076fe076ad30346c720f
Capricious150 <austinandrews89@gmail.com>
2023-11-14 18:03:47 -0700

    Done with navbar for now. Going to start on content layout page. Need ideas for home

--- css_stuff/src/app/tbd/page.tsx
+++ css_stuff/src/app/tbd/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage () {
 
     return (
         <div>
-            <h1>I'm gonna put a website here later</h1>
+            <h1 className="white m2">I'm gonna put a website here later</h1>
         </div>
     )
 }
--- css_stuff/src/components/Navbar.tsx
+++ css_stuff/src/components/Navbar.tsx
@@ -7,17 +7,20 @@ export default function Navbar () {
             <section className="wp20 p1" style={{marginLeft: '1.5rem'}}>
                 <h3 className="yellow">CSS Stuff</h3>
             </section>
-            <section className="wp60 flexRow">
+            <section className="wp70 flexRow">
                 <Link className="white" href='/' style = {{textDecoration: "none"}}>Home</Link>
                     &ensp;
-                <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
+                <Link className="white" href='/content' style = {{textDecoration: "none"}}>Content</Link>
                     &ensp;
                 <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
                     &ensp;
                 <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
             </section>
-            <section className="wp20">
-
+            <section className="wp10 flexRow p1" style={{justifyContent: 'right'}}>
+                {/* Possible future Nav Element, for now just a vanity circle */}
+                <div id="accountCircle" style={{display: "flex", justifyContent: "center", alignItems: "center"}}> 
+                    <h4 className="white">A</h4>
+                </div>
             </section>
         </nav>
     )
