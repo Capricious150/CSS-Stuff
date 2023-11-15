d498d22edff7ad73c315a0c6e7a5b9c8600cf01f
Capricious150 <austinandrews89@gmail.com>
2023-11-14 17:37:36 -0700

    More navbar styling

--- css_stuff/src/app/layout.tsx
+++ css_stuff/src/app/layout.tsx
@@ -1,11 +1,17 @@
 import Navbar from '@/components/Navbar'
 import '../styles/input.scss'
 import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'CSS Stuff',
   description: 'A page for CSS stuff',
 }
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export default function RootLayout({
   children,
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
       <body className="pageBody">
         <Navbar />
         {children}
--- css_stuff/src/components/Navbar.tsx
+++ css_stuff/src/components/Navbar.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link"
 
 export default function Navbar () {
 
     return (
         <nav className = "navBar">
-            
+            <section className="wp20 p1" style={{marginLeft: '1.5rem'}}>
+                <h3 className="yellow">CSS Stuff</h3>
+            </section>
+            <section className="wp60 flexRow">
+                <Link className="white" href='/' style = {{textDecoration: "none"}}>Home</Link>
+                    &ensp;
+                <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
+                    &ensp;
+                <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
+                    &ensp;
+                <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
+            </section>
+            <section className="wp20">
+
+            </section>
         </nav>
     )
 }
