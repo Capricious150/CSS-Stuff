f4f05a26920604cd326ad3c54c0bdfeececcf9a7
Capricious150 <austinandrews89@gmail.com>
2023-11-15 12:43:42 -0700

    Decided I want a dev blog added to the page. This will also let me experiment with blog-style media layout

--- css_stuff/src/components/Navbar.tsx
+++ css_stuff/src/components/Navbar.tsx
@@ -8,13 +8,13 @@ export default function Navbar () {
                 <h3 className="yellow">CSS Stuff</h3>
             </section>
             <section className="wp70 flexRow">
-                <Link className="white" href='/' style = {{textDecoration: "none"}}>Home</Link>
+                <Link className="white hoverDarken" href='/' style = {{textDecoration: "none"}}>Home</Link>
                     &ensp;
-                <Link className="white" href='/content' style = {{textDecoration: "none"}}>Content</Link>
+                <Link className="white hoverDarken" href='/devblog' style = {{textDecoration: "none"}}>Blog</Link>
                     &ensp;
-                <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
+                <Link className="white hoverDarken" href='/content' style = {{textDecoration: "none"}}>Content</Link>
                     &ensp;
-                <Link className="white" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
+                <Link className="white hoverDarken" href='/tbd' style = {{textDecoration: "none"}}>Tbd</Link>
             </section>
             <section className="wp10 flexRow p1" style={{justifyContent: 'right'}}>
                 {/* Possible future Nav Element, for now just a vanity circle */}
